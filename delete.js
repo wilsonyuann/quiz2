a6dc341ebf5615ff52ae8631c562c34d3fa79932
Student <Student>
2017-11-15 21:17:10 +0700

    sukses

---
+++ delete.js
@@ -0,0 +1,46 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var Util = require('util');
+
+var config = {
+  userName: 'abc',
+  password: 'pass123',
+  server: 'localhost',
+  options: {
+      database: 'bootcamp'
+  }
+}
+
+var connection = new Connection(config);
+
+connection.on('connect', function(err) {
+    if (err) {
+        console.log(err)
+    }
+    else{
+        queryDatabase()
+    }
+});
+
+function queryDatabase(){
+
+    console.log('Reading rows from the Table...');
+
+    request = new Request(
+        Util.format("DELETE FROM participant WHERE id = %d", 1002),
+        function(err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+        }
+    );
+
+    request.on('row', function(columns) {
+        console.log(columns);
+    });
+
+    request.on('doneProc', function(){
+      console.log("Querying is done...");
+      process.exit(1);
+    });
+
+    connection.execSql(request);
+}
